3cb4623e7729420ff675e14408ea4de6e146cdab
Diederik Krols <diederik_krols@hotmail.com>
2015-12-28 11:50:51 +0100

    Project initialization: MVVM scaffolding and assets.

---
+++ XamlBrewer.Uwp.Squares/ViewModels/ShellViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+using XamlBrewer.Uwp.Squares;
+
+namespace Mvvm
+{
+    class ShellViewModel : ViewModelBase
+    {
+        public ShellViewModel()
+        {
+            // Build the menu
+            // Symbol enumeration is here: https://msdn.microsoft.com/en-us/library/windows/apps/windows.ui.xaml.controls.symbol.aspx
+            Menu.Add(new MenuItem() { Glyph = Symbol.Edit, Text = "Colors", NavigationDestination = typeof(MainPage) });
+            //Menu.Add(new MenuItem() { Glyph = Symbol.OutlineStar, Text = "Classic", NavigationDestination = typeof(OtherPage) });
+            //Menu.Add(new MenuItem() { Glyph = Symbol.Emoji, Text = "Silly", NavigationDestination = typeof(SillyPage) });
+        }
+    }
+}
