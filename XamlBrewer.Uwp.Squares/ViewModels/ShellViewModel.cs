cf26d679b52385a4acf9ecd36af8e5145fcc55ed
Diederik Krols <diederik_krols@hotmail.com>
2015-12-29 08:30:43 +0100

    Gauge Page.

--- XamlBrewer.Uwp.Squares/ViewModels/ShellViewModel.cs
+++ XamlBrewer.Uwp.Squares/ViewModels/ShellViewModel.cs
@@ -10,7 +10,7 @@ namespace Mvvm
             // Build the menu
             // Symbol enumeration is here: https://msdn.microsoft.com/en-us/library/windows/apps/windows.ui.xaml.controls.symbol.aspx
             Menu.Add(new MenuItem() { Glyph = Symbol.Edit, Text = "Colors", NavigationDestination = typeof(MainPage) });
-            //Menu.Add(new MenuItem() { Glyph = Symbol.OutlineStar, Text = "Classic", NavigationDestination = typeof(OtherPage) });
+            Menu.Add(new MenuItem() { Glyph = Symbol.Rotate, Text = "Gauges", NavigationDestination = typeof(GaugePage) });
             //Menu.Add(new MenuItem() { Glyph = Symbol.Emoji, Text = "Silly", NavigationDestination = typeof(SillyPage) });
         }
     }
---
+++ XamlBrewer.Uwp.Squares/Views/GaugePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using XamlBrewer.Uwp.Controls;
+using WinRTXamlToolkit.Controls;
+
+namespace XamlBrewer.Uwp.Squares
+{
+    public sealed partial class GaugePage : Page
+    {
+        public GaugePage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var square in SquareOfSquares.Squares)
+            {
+                var gauge = new Gauge() { Height = square.ActualHeight, Width = square.ActualWidth };
+                //grid.Background = new SolidColorBrush(square.RandomColor());
+                var side = square.Side();
+                gauge.Value = side;
+                square.Content = gauge;
+            }
+        }
+    }
+}
