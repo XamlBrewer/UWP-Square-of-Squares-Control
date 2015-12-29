c124374bca9236a77d3a4874eeb47627176ee306
Diederik Krols <diederik_krols@hotmail.com>
2015-12-29 08:56:41 +0100

    Clock Page.

--- XamlBrewer.Uwp.Squares/ViewModels/ShellViewModel.cs
+++ XamlBrewer.Uwp.Squares/ViewModels/ShellViewModel.cs
@@ -11,7 +11,7 @@ namespace Mvvm
             // Symbol enumeration is here: https://msdn.microsoft.com/en-us/library/windows/apps/windows.ui.xaml.controls.symbol.aspx
             Menu.Add(new MenuItem() { Glyph = Symbol.Edit, Text = "Colors", NavigationDestination = typeof(MainPage) });
             Menu.Add(new MenuItem() { Glyph = Symbol.Rotate, Text = "Gauges", NavigationDestination = typeof(GaugePage) });
-            //Menu.Add(new MenuItem() { Glyph = Symbol.Emoji, Text = "Silly", NavigationDestination = typeof(SillyPage) });
+            Menu.Add(new MenuItem() { Glyph = Symbol.Clock, Text = "Clocks", NavigationDestination = typeof(ClockPage) });
         }
     }
 }
---
+++ XamlBrewer.Uwp.Squares/Views/ClockPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using XamlBrewer.Uwp.Controls;
+using WinRTXamlToolkit.Controls;
+
+namespace XamlBrewer.Uwp.Squares
+{
+    public sealed partial class ClockPage : Page
+    {
+        public ClockPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (var square in SquareOfSquares.Squares)
+            {
+                var gauge = new Clock() { Height = square.ActualHeight, Width = square.ActualWidth };
+                square.Content = gauge;
+            }
+        }
+    }
+}
